6f0e42caea4737c4ea5e7f479ce853c9870fd405
Wenyi Chen <wenyi@tinystacks.com>
2021-07-20 14:22:59 -0700

    Initial commit of shared eslint config

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: [
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+};
